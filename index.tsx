f681c6b628d0bcda7d31b5ad9bcf17a3bb55481c
Mutsurini <simonedespirito99@gmail.com>
2026-02-17 13:26:40 +0100

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,19 +1,15 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
-
-console.log('SMIRT: Avvio applicazione...');
+import './index.css';
 
 const container = document.getElementById('root');
 
-if (!container) {
-  console.error('Errore critico: Elemento #root non trovato nel DOM.');
-} else {
+if (container) {
   const root = createRoot(container);
   root.render(
     <React.StrictMode>
       <App />
     </React.StrictMode>
   );
-  console.log('SMIRT: React mount completato.');
 }
