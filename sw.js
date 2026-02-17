26b26eccb0a6d1ae5ea3c25c562da3b570d9f51b
Mutsurini <simonedespirito99@gmail.com>
2026-02-17 14:06:32 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,21 @@
+// Service Worker SMIRT - Reset Cache
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          return caches.delete(cacheName);
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  // Lascia che le richieste passino normalmente alla rete
+  return;
+});
